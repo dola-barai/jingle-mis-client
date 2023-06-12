13f0da56141f781f28a2bc5841ad1b8df737521a
Dola Barai <dola43dc@gmail.com>
2023-06-12 13:19:57 +0600

    light and dark theme added

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,14 +1,29 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
 import Footer from "../Shared/Footer/Footer";
+import { useState } from "react";
+import './Main.css';
+import { FaToggleOff, FaToggleOn } from "react-icons/fa";
 
 const Main = () => {
+    const [isDarkTheme, setIsDarkTheme] = useState(false);
+    const toggleTheme = () => {
+        setIsDarkTheme(!isDarkTheme);
+    };
     return (
         <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
+            <div className={`theme-container ${isDarkTheme ? 'dark-theme' : 'light-theme'}`}>
+                <div className="text-center z-10">
+                {
+                    isDarkTheme ? <button onClick={toggleTheme}><FaToggleOff className="h-6 w-6"></FaToggleOff></button> :
+                    <button onClick={toggleTheme}><FaToggleOn className="h-6 w-6"></FaToggleOn></button>
+                }
+                    <Navbar></Navbar>
+                    <Outlet></Outlet>
+                    <Footer></Footer>
+                </div>
+            </div>
 
-            <Footer></Footer>
         </div>
     );
 };
